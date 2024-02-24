34eaa2774836c83fa578ba7f33f567129c76b449
sumanborra <nxtwave@ip-10-2-56-146.ap-south-1.compute.internal>
2024-02-24 11:30:16 +0000

    COOKIES ERROR

---
+++ src/components/Cart/index.js
@@ -0,0 +1,26 @@
+// Write your JS code here
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="Products-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+          alt="cart"
+          className="product-image"
+        />
+      </div>
+    </>
+  )
+}
+export default Cart
